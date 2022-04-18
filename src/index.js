d9ee4218ebaf9bd613c69494bfa8723e9920a572
Jake Edgar <jakeedgar1012@gmail.com>
2022-04-18 12:51:14 -0700

    add basic API call functionality

--- src/index.js
+++ src/index.js
@@ -2,4 +2,27 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
-const url = `https://opentdb.com/api.php?amount=10;`
+
+
+$(document).ready(function() {
+  $('#trivia').click(function() {
+
+    let request = new XMLHttpRequest();
+    const url = `https://opentdb.com/api.php?amount=6&type=boolean`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+    getElements(response);
+    
+    function getElements(response) {
+      $(".text1Showing").text("the answer is ${answer.main.}")
+    }
+  });
+});
